81242f3f2c1f8170ffa13f449ceb79d505f0ac70
Matthew Riley <mgriley97@gmail.com>
2023-08-27 10:02:20 -0400

    Cleaned up the about links

--- JungleReader/src/components/AboutView.vue
+++ JungleReader/src/components/AboutView.vue
@@ -1,39 +1,44 @@
 <script setup>
-
 </script>
 
 <template>
   <div class="AboutView">
-    <h1>About</h1>
-    <p class="Emphasis">
-    JungleReader is a free+open-source web reader.
-    </p>
-    <p>
-    It's made to help you keep up with the sites you like,
-    whatever they may be.
+    <div class="MainInfo">
+      <h1>About</h1>
+      <p class="Emphasis">
+      JungleReader is a free+open-source web reader.
+      </p>
+      <p>
+      It's made to help you keep up with the sites you like,
+      whatever they may be.
 
-    Out of the box, it supports RSS, Mastodon, YouTube, and more. It has a flexible plugin system designed to let devs
-    add support for almost any other site or data source.
-    </p>
-    <p>
-    Your data and reader config are stored locally in your browser. The data should stay
-    around even if you clear your browser's cookies / data, but make sure to export your config sometimes. Cloud backup & sync is coming soon.
-    </p>
-    <h4 class="MoreInfo">More</h4>
-    <p>
-    I made this site as a hobby project, hope you like it! I have more 
-    web related hobby projects planned (JungleWriter, JungleServer, JungleMask, ...), so stay tuned. Here's a newletter
-    if you'd like to follow along with development:
-    </p>
-    <div class="ml-embedded Newsletter" data-form="KvDpFc"></div>
-    <p>
-    You can also leave feedback, report bugs, or request features <a href="https://forms.gle/HqavrHa7jQs4aRbd8" target="_blank" class="LinkButton">here</a>.
-    </p>
+      Out of the box, it supports RSS, Mastodon, YouTube, and more. It has a flexible plugin system designed to let devs
+      add support for almost any other site or data source.
+      </p>
+      <p>
+      Your data and reader config are stored locally in your browser. The data should stay
+      around even if you clear your browser's cookies / data, but make sure to export your config sometimes. Cloud backup & sync is coming soon.
+      </p>
+    </div>
+    <div class="MoreInfo">
+      <h4>More</h4>
+      <p>
+      I develop this site in my spare time, hope you like it! I have more 
+      open-source web projects planned (JungleWriter, JungleServer, JungleWire, JungleMask, ...), so stay tuned.
+      </p>
+      <div class="ExtraWidgets">
+        <ul>
+          <li><p><a class="LinkButton" href="https://forms.gle/HqavrHa7jQs4aRbd8" target="_blank">Leave feedback, report bugs, or request features</a></p></li>
+          <li><p><a class="LinkButton" href="https://dashboard.mailerlite.com/forms/572243/97615844706617024/share" target="_blank">Subscribe to Development Newsletter</a></p></li>
+          <li><p><a class="LinkButton" href="https://github.com/sponsors/mgriley" target="_blank">Sponsor on GitHub</a></p></li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped>
-.AboutView p {
+.MainInfo p {
   margin-bottom: 20px;
 }
 
@@ -43,7 +48,7 @@
 }
 
 .MoreInfo {
-  margin-top: 80px;
+  margin-top: 60px;
 }
 
 .Other {
@@ -52,6 +57,14 @@
 
 .Newsletter {
   margin-bottom: 20px;
+}
+
+.SponsorFrame {
+  margin-top: 20px;
+}
+
+.ExtraWidgets {
+  margin-top: 40px;
 }
 
 </style>
---
+++ JungleReader/src/components/NewsletterForm.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+
+</script>
+
+<template>
+<a class="LinkButton" href="https://dashboard.mailerlite.com/forms/572243/97615844706617024/share" target="_blank">Subscribe to Development Newsletter</a>
+</template>
+
+<style scoped>
+</style>
