d3150f876203437dada67c8666bf3edc20b6ae3f
Matthew Riley <mgriley97@gmail.com>
2023-12-18 21:09:19 -0500

    Tried to improve bg img

--- JungleReader/src/components/Reader.vue
+++ JungleReader/src/components/Reader.vue
@@ -61,81 +61,59 @@ onMounted(() => {
   });
 });
 
-let asciiBg = `
-    ___  ___  ___  ________   ________  ___       _______       
-   |\  \|\  \|\  \|\   ___  \|\   ____\|\  \     |\  ___ \      
-   \ \  \ \  \\\  \ \  \\ \  \ \  \___|\ \  \    \ \   __/|     
- __ \ \  \ \  \\\  \ \  \\ \  \ \  \  __\ \  \    \ \  \_|/__   
-|\  \\_\  \ \  \\\  \ \  \\ \  \ \  \|\  \ \  \____\ \  \_|\ \  
-\ \________\ \_______\ \__\\ \__\ \_______\ \_______\ \_______\ 
- \|________|\|_______|\|__| \|__|\|_______|\|_______|\|_______| 
-                                                                
-                                                                
-                                                                
- ________  _______   ________  ________  _______   ________     
-|\   __  \|\  ___ \ |\   __  \|\   ___ \|\  ___ \ |\   __  \    
-\ \  \|\  \ \   __/|\ \  \|\  \ \  \_|\ \ \   __/|\ \  \|\  \   
- \ \   _  _\ \  \_|/_\ \   __  \ \  \ \\ \ \  \_|/_\ \   _  _\  
-  \ \  \\  \\ \  \_|\ \ \  \ \  \ \  \_\\ \ \  \_|\ \ \  \\  \| 
-   \ \__\\ _\\ \_______\ \__\ \__\ \_______\ \_______\ \__\\ _\ 
-    \|__|\|__|\|_______|\|__|\|__|\|_______|\|_______|\|__|\|__|
-                                                                
-                                                                
-                                                                
-
-`
-
 </script>
 
 <template>
-  <div class="BodyOverlay">
-    <!-- <div class="AsciiBg">{{asciiBg}}</div> -->
-    <div class="toplevel">
-      <div class="Sidebar">
-        <div class="InnerSidebar">
-          <div class="SidebarBtnBar Flex">
-            <button class="SidebarBtn" @click="toggleMobileMenu">
-              JungleReader
-              <!-- <vue-feather class="MenuIcon" id="SidebarMenuIcon" :type="menuOpen ? 'x' : 'menu'" size="24" stroke-width="3" stroke-linecap="butt"></vue-feather> -->
-            </button>
-            <button class="SidebarMenuBtn" @click="toggleMobileMenu">
-              <vue-feather class="MenuIcon" id="SidebarMenuIcon" :type="menuOpen ? 'x' : 'menu'" size="24" stroke-width="3" stroke-linecap="butt"></vue-feather>
-            </button>
-          </div>
-          <div class="SidebarContent" :class="{'open': menuOpen}" id="SidebarContent">
-            <div class="HeroDiv">
-              <p class="AppTitle" @click="goToHome">Jungle<br><span class="LastLine">Reader</span></p>
-              <!-- <img class="HeroImg" src="../assets/NounCham.svg" /> -->
+  <div class="ContainerLayer">
+    <div class="FixedBgImg"></div>
+    <div class="BodyOverlay">
+      <div class="toplevel">
+        <div class="Sidebar">
+          <div class="InnerSidebar">
+            <div class="SidebarBtnBar Flex">
+              <button class="SidebarBtn" @click="toggleMobileMenu">
+                JungleReader
+                <!-- <vue-feather class="MenuIcon" id="SidebarMenuIcon" :type="menuOpen ? 'x' : 'menu'" size="24" stroke-width="3" stroke-linecap="butt"></vue-feather> -->
+              </button>
+              <button class="SidebarMenuBtn" @click="toggleMobileMenu">
+                <vue-feather class="MenuIcon" id="SidebarMenuIcon" :type="menuOpen ? 'x' : 'menu'" size="24" stroke-width="3" stroke-linecap="butt"></vue-feather>
+              </button>
             </div>
-            <div class="SideMenu">
-              <div class="Section">
-                <router-link to="/" id="HomeLink" class="MainLink"><vue-feather type="home" class="Icon"/>Home</router-link>
-                <router-link to="/explore" class="MainLink"><vue-feather type="compass" class="Icon"/>Explore</router-link>
+            <div class="SidebarContent" :class="{'open': menuOpen}" id="SidebarContent">
+              <div class="HeroDiv">
+                <p class="AppTitle" @click="goToHome">Jungle<br><span class="LastLine">Reader</span></p>
+                <!-- <img class="HeroImg" src="../assets/NounCham.svg" /> -->
               </div>
-              <div class="Section">
-                <router-link to="/plugins" class="MainLink"><vue-feather type="plus-square" class="Icon"/>Plugins</router-link>
-                <router-link to="/settings" class="MainLink"><vue-feather type="settings" class="Icon"/>Settings</router-link>
-              </div>
-              <div class="Section">
-                <a href="#" @click.prevent="startImportConfig()" class="MainLink"><vue-feather type="download" class="Icon"/>Import Config</a>
-                <a href="#" @click.prevent="startExportConfig()" class="MainLink"><vue-feather type="download" class="Icon"/>Download Config</a>
-              </div>
-              <div class="Section">
-                <router-link to="/about" class="MainLink"><vue-feather type="info" class="Icon" />About</router-link>
-                <router-link to="/addsupport" class="MainLink"><vue-feather type="plus" class="Icon" />Add Support</router-link>
-                <router-link to="/privacypolicy" class="MainLink"><vue-feather type="shield" class="Icon" />Privacy Policy</router-link>
-                <a href="https://forms.gle/HqavrHa7jQs4aRbd8" target="_blank" class="MainLink"><vue-feather type="send" class="Icon" />Report Bug <!--<vue-feather type="external-link" size="16" />!--></a>
-                <a href="https://github.com/mgriley/BigOlJungle" target="_blank" class="MainLink"><vue-feather type="github" class="Icon" />GitHub <!--<vue-feather type="external-link" size="16" />!--></a>
-              </div>
-              <div class="Section VersionSection">
-                <p class="VersionNum">Version {{ kReaderVersionString }}</p>
+              <div class="SideMenu">
+                <div class="Section">
+                  <router-link to="/" id="HomeLink" class="MainLink"><vue-feather type="home" class="Icon"/>Home</router-link>
+                  <router-link to="/explore" class="MainLink"><vue-feather type="compass" class="Icon"/>Explore</router-link>
+                </div>
+                <div class="Section">
+                  <router-link to="/plugins" class="MainLink"><vue-feather type="plus-square" class="Icon"/>Plugins</router-link>
+                  <router-link to="/settings" class="MainLink"><vue-feather type="settings" class="Icon"/>Settings</router-link>
+                </div>
+                <div class="Section">
+                  <a href="#" @click.prevent="startImportConfig()" class="MainLink"><vue-feather type="download" class="Icon"/>Import Config</a>
+                  <a href="#" @click.prevent="startExportConfig()" class="MainLink"><vue-feather type="download" class="Icon"/>Download Config</a>
+                </div>
+                <div class="Section">
+                  <router-link to="/about" class="MainLink"><vue-feather type="info" class="Icon" />About</router-link>
+                  <router-link to="/addsupport" class="MainLink"><vue-feather type="plus" class="Icon" />Add Support</router-link>
+                  <router-link to="/privacypolicy" class="MainLink"><vue-feather type="shield" class="Icon" />Privacy Policy</router-link>
+                  <a href="https://forms.gle/HqavrHa7jQs4aRbd8" target="_blank" class="MainLink"><vue-feather type="send" class="Icon" />Report Bug <!--<vue-feather type="external-link" size="16" />!--></a>
+                  <a href="https://github.com/mgriley/BigOlJungle" target="_blank" class="MainLink"><vue-feather type="github" class="Icon" />GitHub <!--<vue-feather type="external-link" size="16" />!--></a>
+                </div>
+                <div class="Section VersionSection">
+                  <p class="VersionNum">Version {{ kReaderVersionString }}</p>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-      <div class="main">
-        <router-view></router-view>
+        <div class="main">
+          <router-view></router-view>
+        </div>
       </div>
     </div>
   </div>
@@ -206,17 +184,28 @@ let asciiBg = `
   color: red;
 }
 
-.BodyOverlay {
-  /*backdrop-filter: grayscale(50%);*/
-  /*backdrop-filter: hue-rotate(60deg) brightness(100%);*/
-  backdrop-filter: brightness(400%);
+/*
+Note: this somewhat odd page structure is because chrome was having flicker issues
+  with background images with background-attachment fixed.
+*/
+.FixedBgImg {
+  position: fixed;
+  z-index: -1;
+  background-image: url(../assets/CityBgModded.jpg);
+  background-size: cover;
+  /* background-attachment: fixed; */
+  /* background-repeat: no-repeat; */
+  /* background-position: top; */
+  background-position: 50% 25%;
+  width: 100%;
   min-height: 100vh;
 }
 
-.AsciiBg {
-  font-family: 'Courier New';
-  z-index: -10;
-  white-space: pre;
+.BodyOverlay {
+  /*backdrop-filter: grayscale(50%);*/
+  /*backdrop-filter: hue-rotate(60deg) brightness(100%);*/
+  /* backdrop-filter: brightness(400%); */
+  min-height: 100vh;
 }
 
 .toplevel {
