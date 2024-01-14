3cde935be93790005588cffafb98e1c0e2a00acb
Matthew Riley <mgriley97@gmail.com>
2024-01-14 18:25:52 -0500

    Added changes for PWA

--- JungleReader/vite.config.js
+++ JungleReader/vite.config.js
@@ -2,11 +2,51 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,jpg,svg}']
+      },
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        name: 'JungleReader',
+        short_name: 'JungleReader',
+        description: 'A free and open-source feed-reader. Follow YouTube, Reddit, Mastodon, blogs, RSS, and more.',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            "src": "pwa-64x64.png",
+            "sizes": "64x64",
+            "type": "image/png"
+          },
+          {
+            "src": "pwa-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "pwa-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          },
+          {
+            "src": "maskable-icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable"
+          }
+        ],
+      }
+    })
   ],
   resolve: {
     alias: {
