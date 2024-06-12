a6bfe9a8dd6bfd3b85e772e2af52129c451c00d4
Matthew Riley <mgriley97@gmail.com>
2024-06-12 18:12:47 -0400

    Prog

--- JungleReader/dev-dist/sw.js
+++ JungleReader/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.a3c97r0hl7"
+    "revision": "0.j49a2lhoqn8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
