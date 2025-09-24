c046dd342781d9beec608ac95d53602f566a3678
Matthew Riley <mgriley97@gmail.com>
2025-09-24 14:51:20 -0400

    Prog

---
+++ JungleWriter/src/components/editor/StaticSiteTemplates.js
@@ -0,0 +1,21 @@
+
+export let StaticIndexHtml = `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <!-- TODO - favicons -->
+  <!--<link rel="icon" href="/favicon.svg" type="image/svg+xml">-->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>{{SITE TITLE}}</title>
+  <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+  <div id="app">
+    {{CONTENT}}
+  </div>
+</body>
+</html>
+`;
+
+export let StaticStylesCss = `
+`
