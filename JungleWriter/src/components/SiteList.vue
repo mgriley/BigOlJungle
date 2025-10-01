e430c79bc6db5a8c23cb03b3e503aff925854b6a
Matthew Riley <mgriley97@gmail.com>
2025-10-01 15:38:25 -0400

    Prog

--- JungleWriter/src/components/SiteList.vue
+++ JungleWriter/src/components/SiteList.vue
@@ -62,51 +62,65 @@ async function onFileSelected(event) {
 </script>
 
 <template>
-  <div class="Container">
+  <div class="Toplevel">
     <div class="SiteList">
-      <h1 class="PageHeader">Site List</h1>
+      <h1 class="Title mb-l">JungleWriter 🌴🐒</h1>
       <div class="MarginBotS">
-        <button @click="addSite">Add Site</button>
-        <button @click="importSite">Import</button>
+        <button @click="addSite" class="mr-xxs">New Site</button>
+        <button @click="importSite" class="mr-xs">Import</button>
         <button @click="showDevView" class="TertiaryButton">Developer Tools</button>
       </div>
       <div v-for="site in gApp.sites" :key="site.id" class="SiteItem Flex" @click="gApp.openSiteWithId(site.id)">
         <p class="SiteName">{{ site.name ? site.name : "Untitled" }}</p>
         <p class="SiteId">Id: {{ site.id }}</p>
       </div>
-    </div>
-    <BasicModal class="CreateSiteModal" ref="createSiteModal" title="Create Site"
-      doneText="Create" @onCancel="onCancelAddSite" @onDone="onDoneAddSite">
-      <div v-if="siteToAdd">
-        <div class="FormFieldName">Name</div>
-        <div>
-          <input class="BasicTextInput" v-model="siteToAdd.name" type="text" autofocus>
+      <BasicModal class="CreateSiteModal" ref="createSiteModal" title="Create Site"
+        doneText="Create" @onCancel="onCancelAddSite" @onDone="onDoneAddSite">
+        <div v-if="siteToAdd">
+          <div class="FormFieldName">Name</div>
+          <div>
+            <input class="BasicTextInput" v-model="siteToAdd.name" type="text" autofocus>
+          </div>
         </div>
-      </div>
-    </BasicModal>
-    <BasicModal class="DevModal" ref="devModal" title="Developer Tools"
-      :showDone="false" cancelText="Close" @onCancel="onCloseDevView">
-      <DevView />
-    </BasicModal>
-    <input 
-      ref="fileInput" 
-      type="file" 
-      accept=".zip" 
-      @change="onFileSelected" 
-      style="display: none;"
-    />
+      </BasicModal>
+      <BasicModal class="DevModal" ref="devModal" title="Developer Tools"
+        :showDone="false" cancelText="Close" @onCancel="onCloseDevView">
+        <DevView />
+      </BasicModal>
+      <input 
+        ref="fileInput" 
+        type="file" 
+        accept=".zip" 
+        @change="onFileSelected" 
+        style="display: none;"
+      />
+    </div>
   </div>
 </template>
 
 <style scoped>
+.Toplevel {
+  height: 100vh;
+  padding: var(--space-xl);
+  background-color: rgb(28, 28, 230);
+}
+
 .SiteList {
   margin: 0 auto;
-  margin-top: var(--space-xl);
-  background-color: var(--input-bg);
+  background-color: rgb(28, 28, 230);
   padding: 16px;
   border: var(--border-reg);
   /* border-radius: var(--border-radius-l); */
   max-width: 800px;
+  height: 100%;
+  overflow: auto;
+}
+
+.Title {
+  text-align: center;
+  width: 100%;
+  margin-left: auto;
+  margin-right: auto;
 }
 
 .SiteItem {
