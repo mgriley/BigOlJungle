b86010ae714bb0cffee318d89f74c9846e89f539
Matthew Riley <mgriley97@gmail.com>
2025-10-04 20:37:44 -0400

    Prog

--- JungleWriter/src/components/widgets/NodeWidget.vue
+++ JungleWriter/src/components/widgets/NodeWidget.vue
@@ -13,9 +13,10 @@ const props = defineProps({
 })
 
 let elementRef = ref(null);
+let groupIndicatorRef = ref(null);
 
 onMounted(() => {
-  setupWidgetDrag(elementRef.value, props.node);
+  setupWidgetDrag(groupIndicatorRef.value, props.node);
 })
 
 let styleObject = computed(() => {
@@ -23,24 +24,44 @@ let styleObject = computed(() => {
   let curStyle = {
     width: '0px',
     height: '0px',
+    // Reset the outline, which may be set to red for selected nodes
+    outline: 'none',
   }
+  /*
   if (props.node.isSelected()) {
     let boxLen = 30;
     curStyle.width = boxLen+'px';
     curStyle.height = boxLen+'px';
-    /*curStyle.background = 'black';*/
     curStyle.outline = "2px solid black";
   }
+  */
   return {
     ...baseStyle,
     ...curStyle
   }
 })
 
+let groupIndicatorObject = computed(() => {
+  let style = {
+    width: '0px',
+    height: '0px',
+  }
+  if (props.node.isSelected()) {
+    let boxLen = 30;
+    style.width = boxLen+'px';
+    style.height = boxLen+'px';
+    style.outline = "2px solid black";
+  }
+  return style;
+})
+
 </script>
 
 <template>
   <div class="Widget NodeWidget" ref="elementRef" :style="styleObject">
+    <!-- Use a separate indicator for the drag handle so that we can display it on top 
+     without messing up the node hierarchy. -->
+    <div class="GroupIndicator" ref="groupIndicatorRef" :style="groupIndicatorObject" ></div>
     <template v-for="childNode in node.getChildrenInHtmlOrder()" :key="childNode.id">
       <component v-if="childNode.type !== null" :is="gNodeDataMap[childNode.type].widget" :node="childNode" />
     </template>
@@ -48,6 +69,12 @@ let styleObject = computed(() => {
 </template>
 
 <style scoped>
+.GroupIndicator {
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 1000;
+}
 </style>
 
 <style>
