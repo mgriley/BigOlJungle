0ff5be757f56db32e37b21ae5f71bc1d02ed270e
Matthew Riley <mgriley97@gmail.com>
2025-10-04 21:06:25 -0400

    Prog

--- JungleWriter/src/components/widgets/NodeWidget.vue
+++ JungleWriter/src/components/widgets/NodeWidget.vue
@@ -41,27 +41,16 @@ let styleObject = computed(() => {
   }
 })
 
-let groupIndicatorObject = computed(() => {
-  let style = {
-    width: '0px',
-    height: '0px',
-  }
-  if (props.node.isSelected()) {
-    let boxLen = 30;
-    style.width = boxLen+'px';
-    style.height = boxLen+'px';
-    style.outline = "2px solid black";
-  }
-  return style;
-})
-
 </script>
 
 <template>
   <div class="Widget NodeWidget" ref="elementRef" :style="styleObject">
     <!-- Use a separate indicator for the drag handle so that we can display it on top 
      without messing up the node hierarchy. -->
-    <div class="GroupIndicator" ref="groupIndicatorRef" :style="groupIndicatorObject" ></div>
+    <div class="GroupIndicator" ref="groupIndicatorRef" ></div>
+    <svg class="arrow" width="40" height="20" viewBox="0 0 40 20">
+      <polygon points="0,0 30,10 0,20" fill="currentColor" />
+    </svg>
     <template v-for="childNode in node.getChildrenInHtmlOrder()" :key="childNode.id">
       <component v-if="childNode.type !== null" :is="gNodeDataMap[childNode.type].widget" :node="childNode" />
     </template>
@@ -74,6 +63,28 @@ let groupIndicatorObject = computed(() => {
   top: 0;
   left: 0;
   z-index: 1000;
+  /*
+  width: 20px;
+  height: 20px;
+  */
+  border: 2px solid black;
+  border-right: none;
+  border-bottom: none;
+
+  width: 200px;
+  height: 200px;
+  background-image: url('../../assets/Axis.png');
+  background-repeat: no-repeat;
+  
+  /* Move the background so its center aligns with top-left */
+  background-position: left top; /* sets reference point */
+  
+  /* Adjust using calc() to shift the image by half its size */
+  background-position: calc(0% + 50%) calc(0% + 50%);
+}
+
+.arrow {
+  color: red;
 }
 </style>
 
