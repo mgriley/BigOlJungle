a0ca50220c42afdb332e68162235f2fdb99118da
Matthew Riley <mgriley97@gmail.com>
2025-10-12 01:21:56 -0400

    Prog

--- SkyLizard/site/src/AppManager.js
+++ SkyLizard/site/src/AppManager.js
@@ -1,6 +1,62 @@
 import { reactive } from "vue";
 
+export class DownloadInfo {
+  constructor() {
+    this.url = "";
+  }
+}
+
+export class AppInfo {
+  constructor() {
+    this.idString = "TODO";
+  }
+
+  static create(...args) {
+    return reactive(new AppInfo(...args));
+  }
+}
+
+/**
+ * Manages app stuff
+ */
 export class AppManager {
   constructor() {
+    this.runningApps = reactive([]);
+  }
+
+  async getMyApps() {
+    // TODO
+  }
+
+  async addApp(appInfo) {
+    // TODO
+  }
+
+  async changeAppId(appId, newId) {
+    // TODO
+  }
+
+  async deleteApp(appId) {
+  }
+
+  async stopApp(appId) {
+  }
+
+  async startApp(appId) {
+  }
+
+  async exportApp(appId) {
+    // TODO
+  }
+
+  async importApp(appData) {
+    // TODO
+  }
+
+  async checkAppUpdates(appId) {
+    // TODO
+  }
+async updateApp(appId) {
+    // TODO
   }
 }
