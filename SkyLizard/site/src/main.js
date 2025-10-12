5f63acf5de80278b6c9991dd9ffd378c9899f4ca
Matthew Riley <mgriley97@gmail.com>
2025-10-12 00:58:20 -0400

    Prog

--- SkyLizard/site/src/main.js
+++ SkyLizard/site/src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { setGlobalApp } from './Globals.js'
+import { State } from './State.js';
+
+// Setup the JS state
+let jsApp = State.create()
+setGlobalApp(jsApp);
 
 createApp(App).mount('#app')
